bfd55d47ebd82909c42741332258500f92b0380b
Storm <stricklandstorm10@gmail.com>
2018-07-15 11:17:16 -0400

    Mash

---
+++ mash.js
@@ -0,0 +1,23 @@
+function mash(){
+    
+return "houses"
+
+}
+console.log(mash());
+
+function getHome(userargument){
+let houses = ["Mansion", "Shack","House", "Apartment"];
+
+
+return houses[Math.floor(Math.random()*houses.length)];
+}
+// console.log(houses.Random([i]))
+
+function getChildrenCount(){
+
+    
+}
+
+//  Math.floor(Math.random()*100)
+ console.log(getChildrenCount())
+
